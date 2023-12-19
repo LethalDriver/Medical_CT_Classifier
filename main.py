1bcf3c50b527dd344251cc8572c6573dcb678d63
mwdziak <mwdziak@gmail.com>
2023-12-19 10:11:53 +0100

    More descriptive naming

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ from matplotlib import pyplot as plt
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 import keras
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 batch_size = 32
-target_size = (150, 150)
+image_target_size = (150, 150)
 kidneys_dir = 'CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone'
 
 
-train = keras.utils.image_dataset_from_directory(kidneys_dir, image_size=target_size,
-                                                 validation_split=0.2,
-                                                 label_mode='categorical',
-                                                 subset='training', seed=123)
-val = keras.utils.image_dataset_from_directory(kidneys_dir, image_size=target_size,
-                                               label_mode='categorical',
-                                               validation_split=0.2,
-                                               subset='validation', seed=123)
+train_data = keras.utils.image_dataset_from_directory(kidneys_dir, image_size=image_target_size,
+                                                      validation_split=0.2,
+                                                      label_mode='categorical',
+                                                      subset='training', seed=123)
+validation_data = keras.utils.image_dataset_from_directory(kidneys_dir, image_size=image_target_size,
+                                                           label_mode='categorical',
+                                                           validation_split=0.2,
+                                                           subset='validation', seed=123)
 
-train = train.map(lambda x, y: (x / 255, y))
-val = val.map(lambda x, y: (x / 255, y))
+train_data = train_data.map(lambda x, y: (x / 255, y))
+validation_data = validation_data.map(lambda x, y: (x / 255, y))
 
 VGG_model = Sequential()
 
@@ -31,7 +30,7 @@ pretrained_model = keras.applications.VGG16(include_top=False,
 VGG_model.add(pretrained_model)
 VGG_model.add(Flatten())
 VGG_model.add(Dense(512, activation='relu'))
-VGG_model.add(BatchNormalization())  # Batch Normalization layer
+VGG_model.add(BatchNormalization())
 VGG_model.add(Dropout(0.5))
 
 VGG_model.add(Dense(4, activation='softmax'))
@@ -42,8 +41,8 @@ VGG_model.summary()
 VGG_model.compile(optimizer=keras.optimizers.Adam(0.0001),
                   loss=keras.losses.CategoricalCrossentropy(), metrics=["accuracy"])
 
-history = VGG_model.fit(train, epochs=10,
-                        validation_data=val)
+history = VGG_model.fit(train_data, epochs=10,
+                        validation_data=validation_data)
 
 plt.figure(figsize=(12, 4))
 plt.subplot(1, 2, 1)
@@ -54,7 +53,6 @@ plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='upper left')
 
-# Plot training & validation loss values
 plt.subplot(1, 2, 2)
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
