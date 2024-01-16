2962965f60b9d8b12a46ba2444c33bd41be48cd5
mwdziak <mwdziak@gmail.com>
2024-01-16 16:09:57 +0100

    Optimized imports

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from matplotlib import pyplot as plt
-from tensorflow.keras.layers import *
-from tensorflow.keras.models import *
-import keras
+from tensorflow.keras.layers import BatchNormalization, Dense, Dropout, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from keras.applications.vgg16 import VGG16
+from keras.optimizers import Adam
+from keras.losses import CategoricalCrossentropy
 
 
 def load_rescale_images(batch_size=32, image_size=(150, 150), validation_split=0.2,
                         images_dir='CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone'):
-    train_data = keras.utils.image_dataset_from_directory(images_dir, image_size=image_size,
-                                                          batch_size=batch_size,
-                                                          validation_split=validation_split,
-                                                          label_mode='categorical',
-                                                          subset='training', seed=123)
+    train_data = image_dataset_from_directory(images_dir, image_size=image_size,
+                                              batch_size=batch_size,
+                                              validation_split=validation_split,
+                                              label_mode='categorical',
+                                              subset='training', seed=123)
 
-    validation_data = keras.utils.image_dataset_from_directory(images_dir, image_size=image_size,
-                                                               batch_size=batch_size,
-                                                               label_mode='categorical',
-                                                               validation_split=validation_split,
-                                                               subset='validation', seed=123)
+    validation_data = image_dataset_from_directory(images_dir, image_size=image_size,
+                                                   batch_size=batch_size,
+                                                   label_mode='categorical',
+                                                   validation_split=validation_split,
+                                                   subset='validation', seed=123)
 
     train_data = train_data.map(lambda x, y: (x / 255, y))
     validation_data = validation_data.map(lambda x, y: (x / 255, y))
@@ -27,10 +30,10 @@ def load_rescale_images(batch_size=32, image_size=(150, 150), validation_split=0
 def assemble_model(input_shape=(150, 150, 3), num_classes=4, first_dense_neurons=512, dropout=0.5):
     vgg_model = Sequential()
 
-    pretrained_model = keras.applications.VGG16(include_top=False,
-                                                input_shape=input_shape,
-                                                pooling='max', classes=num_classes,
-                                                weights='imagenet')
+    pretrained_model = VGG16(include_top=False,
+                             input_shape=input_shape,
+                             pooling='max', classes=num_classes,
+                             weights='imagenet')
 
     vgg_model.add(pretrained_model)
     vgg_model.add(Flatten())
@@ -69,8 +72,8 @@ def plot_history(history):
 train_data, validation_data = load_rescale_images()
 VGG_model = assemble_model()
 
-VGG_model.compile(optimizer=keras.optimizers.Adam(0.0001),
-                  loss=keras.losses.CategoricalCrossentropy(), metrics=["accuracy"])
+VGG_model.compile(optimizer=Adam(0.0001),
+                  loss=CategoricalCrossentropy(), metrics=["accuracy"])
 
 history = VGG_model.fit(train_data, epochs=10,
                         validation_data=validation_data)
