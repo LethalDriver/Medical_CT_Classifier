964f7673ac15b90689eebed03eeec03b463e1fb2
mwdziak <mwdziak@gmail.com>
2024-01-16 20:42:08 +0100

    Updated pipeline to be more modular

--- kidney_ct_classifier.py
+++ kidney_ct_classifier.py
@@ -1,13 +1,35 @@
+from keras import Sequential
 from keras.optimizers import Adam
-from keras.losses import SparseCategoricalCrossentropy
+from keras.losses import CategoricalCrossentropy
+from keras.src.layers import Rescaling
+
 from utils import load_images, plot_history
 from model import assemble_model
+from keras.applications.vgg16 import VGG16
 
 train_data, validation_data, test_data = load_images('CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone')
-VGG_model = assemble_model(num_classes=4, first_dense_neurons=512)
+
+input_shape = (150, 150, 3)
+
+pretrained_model = VGG16(include_top=False,
+                             input_shape=input_shape,
+                             pooling='max', classes=4,
+                             weights='imagenet')
+
+pretrained_model.trainable = False
+
+VGG_model = Sequential([
+    Rescaling(1. / 255, input_shape=(150, 150, 3)),
+    pretrained_model,
+    assemble_model(num_classes=4,
+                   first_dense_neurons=512,
+                   )
+])
+
+
 
 VGG_model.compile(optimizer=Adam(0.0001),
-                  loss=SparseCategoricalCrossentropy(), metrics=["accuracy"])
+                  loss=CategoricalCrossentropy(), metrics=["accuracy"])
 
 history = VGG_model.fit(train_data, epochs=10,
                         validation_data=validation_data)
--- model.py
+++ model.py
@@ -1,24 +1,25 @@
 from tensorflow.keras.layers import BatchNormalization, Dense, Dropout, Flatten, Rescaling
 from tensorflow.keras.models import Sequential
-from keras.applications.vgg16 import VGG16
+from keras.layers import RandomFlip, RandomRotation, RandomZoom, RandomContrast
 
 
-def assemble_model(num_classes, first_dense_neurons, input_shape=(150, 150, 3), dropout=0.5, activation='softmax'):
-    vgg_model = Sequential()
+def assemble_model(num_classes, first_dense_neurons, dropout=0.5, activation='softmax'):
 
-    pretrained_model = VGG16(include_top=False,
-                             input_shape=input_shape,
-                             pooling='max', classes=num_classes,
-                             weights='imagenet')
+    model = Sequential([
+        Flatten(),
+        Dense(first_dense_neurons, activation='relu'),
+        BatchNormalization(),
+        Dropout(dropout),
+        Dense(num_classes, activation=activation)
+    ])
 
-    vgg_model.add(Rescaling(1. / 255, input_shape=input_shape))
-    vgg_model.add(pretrained_model)
-    vgg_model.add(Flatten())
-    vgg_model.add(Dense(first_dense_neurons, activation='relu'))
-    vgg_model.add(BatchNormalization())
-    vgg_model.add(Dropout(dropout))
+    return model
 
-    vgg_model.add(Dense(num_classes, activation=activation))
-    pretrained_model.trainable = False
 
-    return vgg_model
+def augmentation_pipeline():
+    return Sequential([
+        RandomFlip("horizontal"),
+        RandomRotation(0.1),
+        RandomZoom(0.05),
+        RandomContrast(0.05)
+    ])
